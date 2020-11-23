86b61829ee41a8cc0f9c8861adf01271a0504ea8
Rodrigo Oliveira <rodrigocode4@gmail.com>
2020-11-23 16:17:01 -0300

    refactoring rote character component

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -6,7 +6,7 @@ import Home from 'components/Home'
 import Main from 'components/Main'
 import ListCharacters from 'components/ListCharacters'
 
-import { BrowserRouter, Route, Link, Redirect } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 import { Character } from 'Utils/types'
 type Props = {
@@ -19,9 +19,7 @@ const Layout = ({ characters }: Props) => {
       <GlobalStyle />
       <S.LayoutWrapper>
         <BrowserRouter>
-          <Link to="/">
-            <Home />
-          </Link>
+          <Home />
           <Search />
           <Route
             path="/"
@@ -34,10 +32,14 @@ const Layout = ({ characters }: Props) => {
                     render={() => <ListCharacters characters={characters} />}
                   />
                   <Route
-                    path={`/people/:id`}
-                    component={() => (
-                      <h1 style={{ color: 'red' }}>{characters?.toString()}</h1>
-                    )}
+                    path={`/:id`}
+                    component={() => {
+                      return (
+                        <h1 style={{ color: 'red' }}>
+                          <code>{JSON.stringify(characters)}</code>
+                        </h1>
+                      )
+                    }}
                   />
                 </Main>
               </>
