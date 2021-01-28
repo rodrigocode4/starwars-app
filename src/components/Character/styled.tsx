d926066236373e48c49630b246dda6d2c63e44cc
Rodrigo Oliveira <rodrigocode4@gmail.com>
2021-01-28 18:45:40 -0300

    Add position style of children componets

--- src/components/Character/styled.tsx
+++ src/components/Character/styled.tsx
@@ -2,4 +2,9 @@ import styled from 'styled-components'
 
 export const CharacterWrapper = styled.div`
   display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  margin-top: 69px;
 `
